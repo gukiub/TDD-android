e3d63a4da60e7d65a5dab6ee34abf47b8a3219fa
Gustavo <gu_kiub@hotmail.com>
2023-02-17 16:38:55 -0300

    moving pokedex to another repository

--- Pokedex/app/src/main/java/com/gustavo/pokedex/model/PokemonType.kt
+++
@@ -1,5 +0,0 @@
-package com.gustavo.pokedex.model
-
-data class PokemonType(
-    val name: String
-)
--- Pokedex/app/src/main/java/com/gustavo/pokedex/util/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.gustavo.pokedex.util
-
-class Constants {
-    companion object {
-        const val BASE_URL = "https://pokeapi.co/api/v2/"
-    }
-}
